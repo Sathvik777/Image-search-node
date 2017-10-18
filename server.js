91c3892c075f650e0d69b82600fa351583e29249
Glitch (fern-sing) <none>
2017-10-18 11:14:04 +0000

    added new bing image search

--- server.js
+++ server.js
@@ -5,7 +5,9 @@
 // init project
 var express = require('express');
 var app = express();
-
+const fetch = require("node-fetch");
+let https = require('https');
+const serch_url = "https://pixabay.com/api/?key=3555577-4bc9f32ecf774a744e7cb48f3";
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
@@ -13,10 +15,24 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile("UNDER DEVELOPMENT");
+app.get("/images/:query", function (request, response) {
+  var query = request.params.query;
+  var off_set = request.query.offset;
+  var query_join = query.replace(/ /g, '+');
+  var serch_final_url = serch_url+"&q="+query_join;
+  console.log(serch_final_url);
+  fetch(serch_final_url)
+  .then(web_response => {
+    web_response.json().then(json => {
+      console.log(json);
+    });
+  })
+  .catch(error => {
+    console.log(error);
+  });
+  //console.log(web_response);
+  response.send("UNDER DEVELOPMENT");
 });
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
